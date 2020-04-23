2366339cd2ad817d2c42145824e3859104af0734
Shagrid <shagrid@yandex.ru>
2020-04-23 10:05:32 +0300

    Исходный файл части 1 урока 1

---
+++ Assets/Scripts/HW1PART1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace DefaultNamespace
+{
+    public class HW1PART1
+    {
+        
+    }
+    
+    public class Customer
+    {
+        public long Id { get; set; }
+        public string Description { get; set; }
+
+        public Customer()
+        {
+            Id = CalculateId();
+        }
+
+        private long CalculateId()
+        {
+            long id = DateTime.Now.Ticks;
+            return id;
+        }
+    }
+
+    public class Store
+    {
+        public long Id { get; set; }
+
+        public Store()
+        {
+            Id = CalculateId();
+        }
+
+        private long CalculateId()
+        {
+            long id = DateTime.Now.Ticks;
+            return id;
+        }
+    }
+
+}
